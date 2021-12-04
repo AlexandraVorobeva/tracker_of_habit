c4c0de25403b07eb8e42af0d2a83356712f07e93
Aleksandra Vorobeva <V-Sash@mail.ru>
2021-12-04 18:20:09 +0300

    habit is finish

--- app/habits/urls.py
+++ app/habits/urls.py
@@ -3,10 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.table_of_week, name='table'),
-    path('home/', views.home, name='home'),
+    path('table/', views.table_of_week, name='table'),
+    path('', views.home, name='home'),
     path('habit/', views.habit, name='habit'),
     path('habit-create/', views.HabitCreate.as_view(), name='habit-create'),
     path('habit-delete/<int:pk>/', views.HabitDelete.as_view(), name='habit-delete'),
     path('week-habit-create/', views.WeekOfHabitCreate.as_view(), name='week-habit-create'),
+    path('group-delete/<int:pk>/', views.GroupOfHabitDelete.as_view(), name='group-delete'),
+    path('group-create/', views.GroupOfHabitCreate.as_view(), name='group-create'),
 ]
--- app/habits/views.py
+++ app/habits/views.py
@@ -21,7 +21,7 @@ def habit(request):
         'groups': GroupOfHabits.objects.all(),
         'habits': Habit.objects.all(),
     }
-    return render(request, 'habit.html', context)
+    return render(request, 'habit_list.html', context)
 
 
 class HabitCreate(CreateView):
@@ -40,6 +40,23 @@ class WeekOfHabitCreate(CreateView):
     model = WeekOfHabit
     fields = ['habit']
     success_url = reverse_lazy('table')
-    template_name = 'week_habit-form.html'
+    template_name = 'table/week_habit_form.html'
+
+
+class GroupOfHabitCreate(CreateView):
+    model = GroupOfHabits
+    fields = ['name']
+    success_url = reverse_lazy('habit')
+    template_name = 'group/group_form.html'
+
+
+class GroupOfHabitDelete(DeleteView):
+    model = GroupOfHabits
+    context_object_name = 'group'
+    success_url = reverse_lazy('habit')
+    template_name = 'group/group_confirm_delete.html'
+
+
+
 
 
